a27cb33e2bf5abfc37f09f4e80e5cd0ad141d6b1
jissuo <jisu2298@gmail.com>
2025-04-02 21:08:08 +0900

    20250402 babel 세팅

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,31 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '~': './src',
+          '@api': './src/api',
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@stores': './src/stores',
+          '@styles': './src/styles',
+        },
+      },
+    ],
+  ],
 };
