046352b5ba80bfdedd7bb4f7fc6ff642be22f7f7
jissuo <jisu2298@gmail.com>
2025-04-02 21:38:47 +0900

    2025402 절대경로로 변경

--- babel.config.js
+++ babel.config.js
@@ -24,6 +24,7 @@ module.exports = {
           '@screens': './src/screens',
           '@stores': './src/stores',
           '@styles': './src/styles',
+          '@type' : './src/types'
         },
       },
     ],
