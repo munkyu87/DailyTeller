a619d0378d54e292ace8cbd53846d1c27580d7df
munkyu87 <munstar87@naver.com>
2025-04-05 23:55:32 +0900

    API스키마 정의

--- src/styles/HomeScreen.styles.ts
+++ src/styles/HomeScreen.styles.ts
@@ -1,19 +1,26 @@
 import {StyleSheet} from 'react-native';
+import colors from '../assets/colors/colors';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: colors.background,
   },
   newsItem: {
     padding: 12,
     borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
+    borderBottomColor: colors.border,
   },
   newsTitle: {
     fontSize: 18,
     fontWeight: 'bold',
+    color: colors.title,
+  },
+  newsDate: {
+    fontSize: 12,
+    color: colors.dateText,
+    marginTop: 4,
   },
 });
 
